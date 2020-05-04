60dc2eff8a89abba88ca89b770bef71dd705466c
Bunny-spec <61130716+Bunny-spec@users.noreply.github.com>
2020-05-05 02:56:47 +0530

    UI updated

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -13,8 +13,24 @@ fetch("http://localhost:3000/weather?address=solapur").then((response) => {
 
 const WeatherForm = document.querySelector("form");
 const search = document.querySelector("input");
+const Message1 = document.querySelector("#msg1");
+const Message2 = document.querySelector("#msg2");
+
 WeatherForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const location = search.value;
-  console.log(location);
+  Message1.textContent = " ";
+  fetch("http://localhost:3000/weather?address=" + location).then(
+    (response) => {
+      response.json().then((data) => {
+        if (data.error) {
+          Message1.textContent = data.error;
+          
+        } else {
+          Message1.textContent = data.location;
+          Message2.textContent = data.forecast;
+        }
+      });
+    }
+  );
 });
