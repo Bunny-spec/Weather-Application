0c1d7a4b2551dfa2049674af7750611caffe133e
Bunny-spec <61130716+Bunny-spec@users.noreply.github.com>
2020-05-07 03:45:48 +0530

    Comment of weather app updated

--- web-server/src/utils/forecast.js
+++ web-server/src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.location.name + ' It is currently ' + body.current.temperature + ' degress out. There is a ' + body.current.precip + '% chance of rain.')
+            callback(undefined, body.location.name + ' is currently ' + body.current.temperature + ' degress out.It is  '+body.current.weather_descriptions[0]+' and there is a ' + body.current.precip + '% chance of rain.The wind speed is estimated to be '+body.current.wind_speed+' nautical miles per hour. Here the current time is '+body.location.localtime+' .')
         }
     })
 }
